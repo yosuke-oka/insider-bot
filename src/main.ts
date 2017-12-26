b24d70f745fdffe5e321d758970b58b3beb9dc40
Yosuke Oka <yosuke.oka@livesense.co.jp>
2017-12-26 16:52:36 +0900

    ping pong

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,23 @@
 import * as Botkit from 'botkit'
 
+declare let process: {
+    env: {
+        TOKEN: string;
+    }
+    exit(status?: number): never
+}
+
+if (!process.env.TOKEN) {
+    console.log('Error: Specify token in environment')
+    process.exit(1)
+}
+
+const controller = Botkit.slackbot({})
+
+const bot = controller.spawn({
+    token: process.env.TOKEN
+}).startRTM()
+
+controller.hears(['ping'], 'direct_message, direct_mention, mention', (bot, message) => {
+    bot.reply(message, 'pong')
+})
